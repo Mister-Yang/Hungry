aca06c548407374ce7798d7811eb671db93e1621
Mister-Yang <yangbosun@creditease.cn>
2019-05-21 19:35:37 +0800

    头部修改

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
 <script>
-	import VHeader from 'components/v-header/v-header.vue'
+import { getSeller } from 'api'
+import VHeader from 'components/v-header/v-header.vue'
 
 export default {
-  name: 'app',
-  components: {
+	name: 'app',
+	data() {
+		return {
+			seller: {}
+		}
+	},
+	created() {
+		this._getSeller()
+	},
+	methods: {
+		_getSeller() {
+			getSeller().then((seller) => {
+				this.seller = seller
+			})
+		}
+	},
+	components: {
 		VHeader
 	}
 }
 </script>
-<style lang="stylus">
-
+<style lang="stylus" scoped>
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
